474215c51824e3498cb9496a9f159b2aef242ef7
vagrant <vagrant@homestead>
2017-06-02 16:13:16 +0000

    added loader for splash fixed bugs

--- resources/views/layouts/home/splash.blade.php
+++ resources/views/layouts/home/splash.blade.php
@@ -3,33 +3,46 @@
         <div class="col-md-12">
             <div class="logo">
                 <img class="img-responsive" src="/images/logo.png">
-            @if ($errors->has('verified'))
-                <span class="help-block-splash">
+                @if ($errors->has('verified'))
+                <p class="help-block-splash">
                     <strong>{{ $errors->first('verified')}}</strong>
-                </span>
-            @endif
-            @if ($errors->has('email'))
-                <span class="help-block-splash">
+                </p>
+                @endif
+                @if ($errors->has('email'))
+                <p class="help-block-splash">
                     <strong>{{ $errors->first('email') }}</strong>
-                </span>
-            @endif
-            @if ($errors->has('password'))
-                <span class="help-block-splash">
+                </p>
+                @endif
+                @if ($errors->has('password'))
+                <p class="help-block-splash">
                     <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
+                </p>
+                @endif
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-md-12">
+            <div class="loader">
+                <div class="spinner">
+                    <div class="dot1">
+                    </div>
+                    <div class="dot2">
+                    </div>
+                </div>
+                <p>
+                    Loading
+                </p>
+            </div>
             <div class="uglyman splash-container">
             </div>
         </div>
+        
     </div>
+</div>
 
-    <div class="arrow bounce">
-        <a class="fa fa-arrow-down fa-2x" href="#ourwork">
-        </a>
-    </div>
+<div class="arrow bounce">
+    <a class="fa fa-arrow-down fa-2x" href="#ourwork">
+    </a>
+</div>
 </div>
