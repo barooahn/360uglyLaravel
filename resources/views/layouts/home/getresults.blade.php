5524c5ec3b7bc05499ddacaa486634562dd3ac6e
vagrant <vagrant@homestead>
2017-07-31 23:51:34 +0000

    splash page design

--- resources/views/layouts/home/getresults.blade.php
+++ resources/views/layouts/home/getresults.blade.php
@@ -1,40 +1,40 @@
 <div class="container-fluid get-results full-height">
-                <div class="text-center get-results-padding">
-                    <h2>
-                        Do you want to
-                        <b>
-                            GET RESULTS
-                        </b>
-                        from your product sales?
-                    </h2>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 col-sm-12 get-results-left">
-                        <img class="img-responsive" src="images/200up.png" alt="200 percent increase in sales"/>
-                    </div>
-                    <div class="col-md-6 col-sm-12">
-                        <h4 class="get-results-text">
-                            Our clients typically get a 200% increase in the number of sales through their new 360 degree product.
-                        </h4>
-                    </div>
-                </div>
+    <div class="text-center get-results-padding">
+        <h2>
+            USE 360 PRODUCT PHOTOGRAPHY TO
+            <b>
+                GET improved RESULTS
+            </b>
+            from your product sales
+        </h2>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-sm-12 get-results-left">
+            <img class="img-responsive" src="images/200up.png" alt="200 percent increase in sales"/>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <h4 class="get-results-text">
+                Our clients typically get a 200% increase in the number of sales through their new 360 degree product.
+            </h4>
+        </div>
+    </div>
 
-                <div class="row">
-                    <div class="col-md-8 col-sm-12 col-md-offset-2">
-                        <div class=" get-results-body">
-                            <h3>Why use 360 web photos?</h3>
+    <div class="row">
+        <div class="col-md-8 col-sm-12 col-md-offset-2">
+            <div class=" get-results-body">
+                <h3>Why use 360 web photos?</h3>
 
-                            <p>360Ugly is a premium service to increase your product sales by using 360 product photography on your website.  360 Images, sometimes called 3D images, can be used to show all angles of your product in a smooth, beautiful, rotation.  Not only does the buyer have more views of the product but they can choose angles they want to see.  Revolving products lead to less product returns and more product sales</p>
+                <p>360 Ugly LTD is a premium web service used to increase your product sales by utilising 360 product photography on your website.  360 Images, sometimes called 3D images, can be used to show all angles of your product in a smooth, beautiful, rotation.  Not only does the buyer have more views of the product but they can choose angles they want to see.  Revolving products lead to less product returns and more product sales</p>
 
-                            <h3>Is it easy to install 360 products on my website?</h3>
+                <h3>Using 360 revolving products on my website</h3>
 
-                            <p>The 360 Images can be uploaded to your website and incorporated with three simple lines of code.  Simply cut and paste the code supplied with your download to your site and you will have 360/3D product displayed.  In this case the 360 product photos are called 360ProductPhoto.  It is as simple as uploading the files and copying / pasting three lines of code similar to the ones below.</p>  
+                <p>The 360 Images can be uploaded to your website and incorporated with three simple lines of code.  Simply cut and paste the code supplied with your download to your site and you will have 360/3D product displayed.  In this case the 360 product photos are called 360ProductPhoto.  It is as simple as uploading the files and copying / pasting three lines of code similar to the ones below.</p>  
 
-                            <pre><code>&lt;script src="randj.js"&gt;&lt;/script&gt;
+                <pre><code>&lt;script src="randj.js"&gt;&lt;/script&gt;
 &lt;div class="360ProductPhoto00020002">&lt;/div&gt;
 &lt;script src=\"360ProductPhoto00020002.js\">&lt;/script&gt;</code></pre>
-                        </div>
-                    </div>
-                </div>
             </div>
-            <!--end container-->
+        </div>
+    </div>
+</div>
+<!--end container-->
